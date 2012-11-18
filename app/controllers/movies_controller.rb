119b33601a36afe96077e5dd9d01d9ca6df71e6e
Matthew Liu <matthewliu@gmail.com>
2012-11-18 14:24:50 -0800

    First commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
 
+  def search_tmdb
+    @movies = Movie.find_in_tmdb(params[:search_terms])
+  end
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,35 @@
 class Movie < ActiveRecord::Base
+
+  class Movie::InvalidKeyError < StandardError ; end
+
+  #Chapter 7 code
+
+  RATINGS = %w[G PG PG-13 R NC-17]  #  %w[] shortcut for array of strings
+  validates :title, :presence => true
+  validates :release_date, :presence => true
+  validate :released_1930_or_later # uses custom validator below
+  validates :rating, :inclusion => {:in => RATINGS}, :unless => :grandfathered?
+  def released_1930_or_later
+    errors.add(:release_date, 'must be 1930 or later') if
+      self.release_date < Date.parse('1 Jan 1930')
+  end
+  def grandfathered? ; self.release_date >= @@grandfathered_date ; end
+  
+  #Chapter 6 coce
+
+  def self.api_key
+    'cc4b67c52acb514bdf4931f7cedfd12b' # replace with YOUR Tmdb key
+  end
+
+  def self.find_in_tmdb(string)
+    Tmdb.api_key = self.api_key
+    begin
+      TmdbMovie.find(:title => string)
+    rescue ArgumentError => tmdb_error
+      raise Movie::InvalidKeyError, tmdb_error.message
+    end
+  end
+  # rest of file elided for brevity
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
